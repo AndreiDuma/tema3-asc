05d293c841de50caa41cc3607be99f25ab73e178
andrei_dorian.duma <andrei_dorian.duma@fep-62-1.grid.pub.ro>
2015-05-12 21:06:14 +0300

    Almost...

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,30 @@
-./ppu/ppu 0 0 8 /export/asc/tema3_input/in1.pgm out.cmp out.pgm
-./ppu/ppu 1 0 8 /export/asc/tema3_input/in1.pgm out.cmp out.pgm
-./ppu/ppu 2 0 8 /export/asc/tema3_input/in1.pgm out.cmp out.pgm
+#!/bin/bash
+
+FILES="1"
+MODE_DMA="0 1"
+MODE_VECT="0 1 2"
+SPU_NUMS="1 2 4 8"
+
+mkdir results
+
+# iterate through files
+for i in $FILES;
+do
+    # iterate over dma modes
+    for mode_dma in $MODE_DMA;
+    do
+        # iterate over vectorization modes
+        for mode_vect in $MODE_VECT;
+        do
+            # iterate over SPU number
+            for num in $SPU_NUMS;
+            do
+                ./ppu/ppu $mode_vect $mode_dma $num /export/asc/tema3_input/in$i.pgm out.cmp out.pgm > results/$i.$mode_dma.$mode_vect.$num.txt
+                ../serial/compare cmp out.cmp /export/asc/tema3_output/out$i.cmp >> results/compare.txt
+                ../serial/compare pgm out.pgm /export/asc/tema3_output/out$i.pgm >> results/compare.txt
+            done
+        done
+    done
+done
+
+# rm -r results
--- spu/spu.c
+++ spu/spu.c
@@ -9,12 +9,14 @@
 #define MAX_SPU_THREADS   8
 
 void blk_print(unsigned char *pixels);
-void compress(struct args *arg, uint32_t tag_id);
-void decompress(struct args *arg, uint32_t tag_id);;
+void compress(struct args *arg, uint32_t tag_id[2]);
+void decompress(struct args *arg, uint32_t tag_id[2]);;
 
 int main(unsigned long long speid, unsigned long long argp, unsigned long long envp) {
-    uint32_t tag_id = mfc_tag_reserve();
-    if (tag_id==MFC_TAG_INVALID){
+    uint32_t tag_id[2];
+    tag_id[0] = mfc_tag_reserve();
+    tag_id[1] = mfc_tag_reserve();
+    if (tag_id[0] == MFC_TAG_INVALID || tag_id[1] == MFC_TAG_INVALID) {
         printf("SPU: ERROR can't allocate tag ID\n");
         return -1;
     }
@@ -22,8 +24,8 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
     struct args arg __attribute__ ((aligned(16)));
 
     /* get the arguments through DMA */
-    mfc_get((void *) &arg, (uint32_t) argp, sizeof(struct args), tag_id, 0, 0);
-    waitag(tag_id);
+    mfc_get((void *) &arg, (uint32_t) argp, sizeof(struct args), tag_id[0], 0, 0);
+    waitag(tag_id[0]);
 
     /* compress or decompress depending on mode_op parameter */
     switch (arg.mode_op) {
@@ -37,57 +39,89 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
     }
 
     /* release tag */
-    mfc_tag_release(tag_id);
+    mfc_tag_release(tag_id[0]);
+    mfc_tag_release(tag_id[1]);
 
     return 0;
 }
 
-void compress(struct args *arg, uint32_t tag_id) {
+void compress(struct args *arg, uint32_t tag_id[2]) {
     /* Get image information */
     struct img image __attribute__ ((aligned(16)));
     struct c_img c_image __attribute__ ((aligned(16)));
-    mfc_get((void *) &image, (uint32_t) arg->image, sizeof(struct img), tag_id, 0, 0);
-    mfc_get((void *) &c_image, (uint32_t) arg->c_image, sizeof(struct c_img), tag_id, 0, 0);
-    waitag(tag_id);
+    mfc_get((void *) &image, (uint32_t) arg->image, sizeof(struct img), tag_id[0], 0, 0);
+    mfc_get((void *) &c_image, (uint32_t) arg->c_image, sizeof(struct c_img), tag_id[0], 0, 0);
+    waitag(tag_id[0]);
 
     /* local pixel block */
-    unsigned char pixels[BLOCK_SIZE * BLOCK_SIZE] __attribute__ ((aligned(16)));
-    struct block blk __attribute__ ((aligned(16)));
+    unsigned char pixels[2][BLOCK_SIZE * BLOCK_SIZE] __attribute__ ((aligned(16)));
+    struct block blk[2] __attribute__ ((aligned(16)));
 
     /* dimensions in blocks and number of blocks */
     int blk_width = image.width / BLOCK_SIZE,
         blk_height = image.height / BLOCK_SIZE,
-        blk_num = blk_width * blk_height,
-        blk_index;
+        blk_num = blk_width * blk_height;
 
-    for (blk_index = arg->spu; blk_index < blk_num; blk_index += arg->spu_num) {
-        /* start row and column of current block */
+    int blk_index, blk_index_next,
+        buf, buf_next;
+
+    /* if double buffering should be used */
+    if (arg->mode_dma) {
+        /* setup double indices and buffers */
+        blk_index = arg->spu, blk_index_next = blk_index + arg->spu_num;
+        buf = 0, buf_next = 1;
+
+        /* start row and column for first transfer */
         int blk_row = blk_index / blk_width * BLOCK_SIZE,
             blk_col = blk_index % blk_width * BLOCK_SIZE;
 
-        /* get block pixels through DMA */
-        int row, col;
+        /* first transfer */
+        int row;
         for (row = 0; row < BLOCK_SIZE; row++) {
             unsigned char *ppu_pixel_row = image.pixels + (blk_row + row) * image.width + blk_col,
-                          *spu_pixel_row = pixels + row * BLOCK_SIZE;
-            mfc_get((void *) spu_pixel_row, (uint32_t) ppu_pixel_row, BLOCK_SIZE, tag_id, 0, 0);
+                          *spu_pixel_row = pixels[buf] + row * BLOCK_SIZE;
+            mfc_get((void *) spu_pixel_row, (uint32_t) ppu_pixel_row, BLOCK_SIZE, tag_id[buf], 0, 0);
         }
-        waitag(tag_id);
+    } else {
+        /* simple buffering */
+        blk_index = arg->spu, blk_index_next = blk_index;
+        buf = 0, buf_next = buf;
+    }
+
+    /* process blocks with identifiers equal to arg->spu modulo arg->spu_num */
+    for (; blk_index < blk_num;
+           blk_index += arg->spu_num, blk_index_next += arg->spu_num,
+           buf ^= arg->mode_dma, buf_next ^= arg->mode_dma) {
+
+        /* start row and column of next block */
+        int blk_row_next = blk_index_next / blk_width * BLOCK_SIZE,
+            blk_col_next = blk_index_next % blk_width * BLOCK_SIZE;
+
+        /* start next transfer */
+        int row, col;
+        for (row = 0; row < BLOCK_SIZE; row++) {
+            unsigned char *ppu_pixel_row = image.pixels + (blk_row_next + row) * image.width + blk_col_next,
+                          *spu_pixel_row = pixels[buf_next] + row * BLOCK_SIZE;
+            mfc_get((void *) spu_pixel_row, (uint32_t) ppu_pixel_row, BLOCK_SIZE, tag_id[buf_next], 0, 0);
+        }
+
+        /* wait for current transfer */
+        waitag(tag_id[buf]);
 
         /* compute minimum and maximum */
-        unsigned char min = pixels[0],
-                      max = pixels[0];
+        unsigned char min = pixels[buf][0],
+                      max = pixels[buf][0];
         unsigned int i;
         for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++){
-            if (pixels[i] < min) {
-                min = pixels[i];
+            if (pixels[buf][i] < min) {
+                min = pixels[buf][i];
             }
-            if (pixels[i] > max) {
-                max = pixels[i];
+            if (pixels[buf][i] > max) {
+                max = pixels[buf][i];
             }
         }
-        blk.min = min;
-        blk.max = max;
+        blk[buf].min = min;
+        blk[buf].max = max;
 
         /* compute factor */
         float factor = (max - min) / (float) (NUM_COLORS_PALETTE - 1);
@@ -100,8 +134,8 @@ void compress(struct args *arg, uint32_t tag_id) {
                 case SCALAR:
                     for (row = 0; row < BLOCK_SIZE; row++){
                         for (col = 0; col < BLOCK_SIZE; col++){
-                            float aux = (pixels[row * BLOCK_SIZE + col] - min) / factor;
-                            blk.index_matrix[row * BLOCK_SIZE + col] = (unsigned char) (aux + 0.5);
+                            float aux = (pixels[buf][row * BLOCK_SIZE + col] - min) / factor;
+                            blk[buf].index_matrix[row * BLOCK_SIZE + col] = (unsigned char) (aux + 0.5);
                         }
                     }
                     break;
@@ -110,7 +144,7 @@ void compress(struct args *arg, uint32_t tag_id) {
                     /* temporary buffer */
                     float fpixels[BLOCK_SIZE * BLOCK_SIZE] __attribute__ ((aligned(16)));
                     for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                        fpixels[i] = pixels[i];
+                        fpixels[i] = pixels[buf][i];
                     }
 
                     vector float *vpixels = (vector float *) fpixels;
@@ -125,7 +159,7 @@ void compress(struct args *arg, uint32_t tag_id) {
                     }
 
                     for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                        blk.index_matrix[i] = (unsigned char) fpixels[i];
+                        blk[buf].index_matrix[i] = (unsigned char) fpixels[i];
                     }
 
                     break;
@@ -139,7 +173,7 @@ void compress(struct args *arg, uint32_t tag_id) {
                     /* convert unsigned char to unsigned int */
                     unsigned char *cpixels = (unsigned char *) fpixels;
                     for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                        cpixels[i * sizeof(unsigned int) + 3] = pixels[i];
+                        cpixels[i * sizeof(unsigned int) + 3] = pixels[buf][i];
                     }
 
                     vector float *vpixels = (vector float *) fpixels;
@@ -163,7 +197,7 @@ void compress(struct args *arg, uint32_t tag_id) {
 
                     /* back to unsigned char */
                     for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                        blk.index_matrix[i] = cpixels[i * sizeof(unsigned int) + 3];
+                        blk[buf].index_matrix[i] = cpixels[i * sizeof(unsigned int) + 3];
                     }
 
                     break;
@@ -171,53 +205,71 @@ void compress(struct args *arg, uint32_t tag_id) {
             }
         } else {
             /* all colors represented with min => index = 0 */
-            memset(blk.index_matrix, 0, BLOCK_SIZE * BLOCK_SIZE);
+            memset(blk[buf].index_matrix, 0, BLOCK_SIZE * BLOCK_SIZE);
         }
 
         /* put the block back in memory */
         struct block *ppu_blk = c_image.blocks + blk_index;
-        mfc_put((void *) &blk, (uint32_t) ppu_blk, sizeof(struct block), tag_id, 0, 0);
-        waitag(tag_id);
+        mfc_put((void *) &blk[buf], (uint32_t) ppu_blk, sizeof(struct block), tag_id[buf], 0, 0);
     }
 }
 
-void decompress(struct args *arg, uint32_t tag_id) {
+void decompress(struct args *arg, uint32_t tag_id[2]) {
     /* Get image information */
     struct c_img c_image __attribute__ ((aligned(16)));
     struct img d_image __attribute__ ((aligned(16)));
-    mfc_get((void *) &c_image, (uint32_t) arg->c_image, sizeof(struct c_img), tag_id, 0, 0);
-    mfc_get((void *) &d_image, (uint32_t) arg->d_image, sizeof(struct img), tag_id, 0, 0);
-    waitag(tag_id);
+    mfc_get((void *) &c_image, (uint32_t) arg->c_image, sizeof(struct c_img), tag_id[0], 0, 0);
+    mfc_get((void *) &d_image, (uint32_t) arg->d_image, sizeof(struct img), tag_id[0], 0, 0);
+    waitag(tag_id[0]);
 
     /* local pixel block */
-    struct block blk;
-    unsigned char pixels[BLOCK_SIZE * BLOCK_SIZE];
+    struct block blk[2] __attribute__ ((aligned(16)));
+    unsigned char pixels[2][BLOCK_SIZE * BLOCK_SIZE] __attribute__ ((aligned(16)));
 
     /* dimensions in blocks and number of blocks */
     int blk_width = c_image.width / BLOCK_SIZE,
         blk_height = c_image.height / BLOCK_SIZE,
-        blk_num = blk_width * blk_height,
-        blk_index;
+        blk_num = blk_width * blk_height;
 
-    for (blk_index = arg->spu; blk_index < blk_num; blk_index += arg->spu_num) {
-        /* start row and column of current block */
-        int blk_row = blk_index / blk_width * BLOCK_SIZE,
-            blk_col = blk_index % blk_width * BLOCK_SIZE;
+    int blk_index, blk_index_next,
+        buf, buf_next;
 
-        /* get block through DMA */
+    /* if double buffering should be used */
+    if (arg->mode_dma) {
+        /* setup double indices and buffers */
+        blk_index = arg->spu, blk_index_next = blk_index + arg->spu_num;
+        buf = 0, buf_next = 1;
+
+        /* first transfer */
         struct block *ppu_blk = c_image.blocks + blk_index;
-        mfc_get((void *) &blk, (uint32_t) ppu_blk, sizeof(struct block), tag_id, 0, 0);
-        waitag(tag_id);
+        mfc_get((void *) &blk[buf], (uint32_t) ppu_blk, sizeof(struct block), tag_id[buf], 0, 0);
+    } else {
+        /* simple buffering */
+        blk_index = arg->spu, blk_index_next = blk_index;
+        buf = 0, buf_next = buf;
+    }
+
+    /* process blocks with identifiers equal to arg->spu modulo arg->spu_num */
+    for (; blk_index < blk_num;
+           blk_index += arg->spu_num, blk_index_next += arg->spu_num,
+           buf ^= arg->mode_dma, buf_next ^= arg->mode_dma) {
+
+        /* get next block */
+        struct block *ppu_blk = c_image.blocks + blk_index_next;
+        mfc_get((void *) &blk[buf_next], (uint32_t) ppu_blk, sizeof(struct block), tag_id[buf_next], 0, 0);
+
+        /* wait for current transfer */
+        waitag(tag_id[buf]);
 
         /* compute factor */
-        float factor = (blk.max - blk.min) / (float) (NUM_COLORS_PALETTE - 1);
+        float factor = (blk[buf].max - blk[buf].min) / (float) (NUM_COLORS_PALETTE - 1);
 
         /* restore pixels by requested method */
         switch (arg->mode_vect) {
             case SCALAR: {
                 unsigned int i;
                 for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++){
-                    pixels[i] = (unsigned char) (blk.min + blk.index_matrix[i] * factor + 0.5);
+                    pixels[buf][i] = (unsigned char) (blk[buf].min + blk[buf].index_matrix[i] * factor + 0.5);
                 }
 
                 break;
@@ -230,11 +282,11 @@ void decompress(struct args *arg, uint32_t tag_id) {
 
                 /* cast to float */
                 for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                    aux[i] = (float) blk.index_matrix[i];
+                    aux[i] = (float) blk[buf].index_matrix[i];
                 }
 
                 vector float vfactor = spu_splats(factor),
-                             vminhalf = spu_splats((float) blk.min + 0.5f);
+                             vminhalf = spu_splats((float) blk[buf].min + 0.5f);
 
                 vector float *vaux = (vector float *) aux;
 
@@ -245,7 +297,7 @@ void decompress(struct args *arg, uint32_t tag_id) {
 
                 /* cast back to unsigned char */
                 for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                    pixels[i] = (unsigned char) aux[i];
+                    pixels[buf][i] = (unsigned char) aux[i];
                 }
 
                 break;
@@ -260,11 +312,11 @@ void decompress(struct args *arg, uint32_t tag_id) {
                 unsigned char *caux = (unsigned char *) aux;
                 unsigned int i;
                 for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                    caux[i * sizeof(unsigned int) + 3] = blk.index_matrix[i];
+                    caux[i * sizeof(unsigned int) + 3] = blk[buf].index_matrix[i];
                 }
 
                 vector float vfactor = spu_splats(factor),
-                             vminhalf = spu_splats((float) blk.min + 0.5f);
+                             vminhalf = spu_splats((float) blk[buf].min + 0.5f);
 
                 vector float *vaux = (vector float *) aux;
                 vector unsigned int *ivaux = (vector unsigned int *) aux;
@@ -282,21 +334,24 @@ void decompress(struct args *arg, uint32_t tag_id) {
 
                 /* back to unsigned char */
                 for (i = 0; i < BLOCK_SIZE * BLOCK_SIZE; i++) {
-                    pixels[i] = caux[i * sizeof(unsigned int) + 3];
+                    pixels[buf][i] = caux[i * sizeof(unsigned int) + 3];
                 }
 
                 break;
             }
         }
 
+        /* start row and column of current block */
+        int blk_row = blk_index / blk_width * BLOCK_SIZE,
+            blk_col = blk_index % blk_width * BLOCK_SIZE;
+
         /* put the pixels back in memory */
         int row;
         for (row = 0; row < BLOCK_SIZE; row++) {
             unsigned char *ppu_pixel_row = d_image.pixels + (blk_row + row) * d_image.width + blk_col,
-                          *spu_pixel_row = pixels + row * BLOCK_SIZE;
-            mfc_put((void *) spu_pixel_row, (uint32_t) ppu_pixel_row, BLOCK_SIZE, tag_id, 0, 0);
+                          *spu_pixel_row = pixels[buf] + row * BLOCK_SIZE;
+            mfc_put((void *) spu_pixel_row, (uint32_t) ppu_pixel_row, BLOCK_SIZE, tag_id[buf], 0, 0);
         }
-        waitag(tag_id);
     }
 }
 
